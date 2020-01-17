e4f62b545db925d578997854f02ea970b58c7848
moe10291 <mohammad57zero@gmail.com>
2020-01-16 23:51:22 -0500

    getting data from API. Making it speak

---
+++ app.js
@@ -0,0 +1,91 @@
+const btn= document.querySelector('.talk');
+const content = document.querySelector('.content')
+//  let long;
+//     let lat;
+//     let tempratureDescription= document.querySelector('.temprature-description');
+//     let tempratureDegree= document.querySelector('.temprature-degree');
+//     let locationTimezone= document.querySelector('.location-timezone');
+//     let tempratureSection= document.querySelector('.temprature');
+//     const tempratureSpan= document.querySelector('.temprature span');
+
+//
+
+const greetings=['I am good', 'Rocking as usual', 'Better than I deserve']
+const weather=['weather is fine', 'its sunny', 'why do you care?']
+const SpeechRecognition= window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition= new SpeechRecognition();
+
+recognition.onstart= function (){
+   console.log('speak up')
+};
+
+recognition.onresult= function (event){
+    const current= event.resultIndex;
+
+    const transcript= event.results[current][0].transcript;
+    console.log(transcript)
+    content.textContent=transcript;
+    
+    readOutLoud(transcript);
+};
+
+//add the listener 
+
+btn.addEventListener('click', ()=> {
+    recognition.start();
+});
+
+
+function readOutLoud(message){
+    const speech= new SpeechSynthesisUtterance();
+    speech.text= ''
+    if(message.includes('weather')){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(position => {
+                lat= position.coords.latitude;
+                long= position.coords.longitude;
+    
+                const proxy= 'http://cors-anywhere.herokuapp.com/'
+    
+                const api= `${proxy}https://api.darksky.net/forecast/4b7f2d7b6781c0bf8f322478f9d100a3/${lat}, ${long}`
+                console.log('IM HERE', api)
+                fetch(api)
+                .then(response =>{
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data)
+                    const {temperature, summary, icon}= data.currently;
+                    console.log('WEATHER', data.currently.temperature)
+                    //Set DOM elements from the API
+    
+                    // tempratureDegree.textContent= Math.round(temperature);
+                    // tempratureDescription.textContent= summary;
+                    // locationTimezone.textContent= data.timezone;
+                    // Formula for Celcius
+    
+                    let celsius= (temperature-32)* (5/9)
+                    console.log('LETS SEE', celsius)
+
+                    const finalText= data.currently.temperature
+                    console.log('WEATHER', finalText)
+                    speech.text= finalText;
+                    console.log(speech.text)
+        
+    })
+
+})}
+
+}
+
+    else if (message.includes('how are you')){
+        const finalText= greetings[Math.floor(Math.random()*greetings.length)];
+        speech.text= finalText;
+    }
+    speech.volume= 1;
+    speech.rate= 1;
+    speech.pitch=1;
+    // speech.text= message;
+
+    window.speechSynthesis.speak(speech);
+}
